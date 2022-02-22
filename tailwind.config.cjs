4cfc269cc0b98a6f11df5788d8676afc01e16b21
Sebastian <sebastian@iamsebastian.dev>
2022-02-22 02:31:53 +0100

    Inital commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @format */
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}', 'index.html'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				mw: 'Merriweather, serif',
+			},
+		},
+	},
+
+	plugins: [],
+};
+
+module.exports = config;
