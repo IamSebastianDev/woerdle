85459980a9806596f0f620643c99d37281124ce6
Sebastian <sebastian@iamsebastian.dev>
2022-02-22 02:34:26 +0100

    PROG: Added fonts & layouts to app

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navigation from './lib/layouts/Navigation.svelte';
+	import Aside from './lib/components/Navigation/Aside.svelte';
+	import Wordle from './lib/layouts/Wordle.svelte';
+
+	import Settings from './lib/components/Modals/Settings.svelte';
+	import Instructions from './lib/components/Modals/Instructions.svelte';
+</script>
+
+<Navigation />
+<Aside />
+<div id="modals">
+	<Settings />
+	<Instructions />
+</div>
+<main>
+	<Wordle />
+</main>
