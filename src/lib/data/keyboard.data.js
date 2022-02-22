39f7d330fcec7e9cfcd76140ef3f80e8b888540c
Sebastian <sebastian@iamsebastian.dev>
2022-02-22 15:03:13 +0100

    PROG: Added keyboard layout

---
+++ src/lib/data/keyboard.data.js
@@ -0,0 +1,38 @@
+/** @format */
+
+export default {
+	row1: [
+		{ char: 'q' },
+		{ char: 'w' },
+		{ char: 'e' },
+		{ char: 'r' },
+		{ char: 't' },
+		{ char: 'y' },
+		{ char: 'u' },
+		{ char: 'i' },
+		{ char: 'o' },
+		{ char: 'p' },
+	],
+	row2: [
+		{ char: 'a' },
+		{ char: 's' },
+		{ char: 'd' },
+		{ char: 'f' },
+		{ char: 'g' },
+		{ char: 'h' },
+		{ char: 'j' },
+		{ char: 'k' },
+		{ char: 'l' },
+	],
+	row3: [
+		{ icon: 'eval' },
+		{ char: 'z' },
+		{ char: 'x' },
+		{ char: 'c' },
+		{ char: 'v' },
+		{ char: 'b' },
+		{ char: 'n' },
+		{ char: 'm' },
+		{ icon: 'delete' },
+	],
+};
---
+++ src/lib/layouts/Keyboard.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { FnReturn, FnBackspace } from 'svelte-pangolicons';
+	import Key from '../components/GamePieces/Key.svelte';
+
+	import keyboard from '../data/keyboard.data.js';
+	import {
+		updateGameState,
+		evaluatedKeys,
+	} from '../stores/gamestate.store.js';
+
+	const keyRows = Object.values(keyboard);
+
+	$: getKeyState = (char) => $evaluatedKeys[char];
+
+	const setKey = (char) => () =>
+		updateGameState({
+			type: !['delete', 'eval'].includes(char) ? 'key' : char,
+			payload: char,
+		});
+</script>
+
+<div class="mx-auto grid h-[25vh] w-full max-w-lg grid-rows-3 py-2">
+	{#each keyRows as row}
+		<div class="mx-2 flex h-full flex-row justify-center pb-1">
+			{#each row as key}
+				<Key
+					on:click={setKey(key.char || key.icon)}
+					state={getKeyState(key.char)}
+				>
+					{#if key.icon === 'eval'}
+						<FnReturn />
+					{:else if key.icon === 'delete'}
+						<FnBackspace />
+					{:else}
+						{key.char.toUpperCase()}
+					{/if}
+				</Key>
+			{/each}
+		</div>
+	{/each}
+</div>
