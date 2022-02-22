4f53aa2db40de2b9d9e8d4902df6cf76c89ee45c
Sebastian <sebastian@iamsebastian.dev>
2022-02-22 23:14:20 +0100

    PROG: Removed 'hard mode' toggle, added refresh button

--- src/lib/components/Modals/Settings.svelte
+++ src/lib/components/Modals/Settings.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Modal from '../UI/Modal.svelte';
-	import { activeModal, closeModal } from '../../stores/modal.store';
+	import { activeModal, closeModal } from '../../stores/modal.store.js';
+	import { resetGameState } from '../../stores/gamestate.store.js';
 
 	import Toggle from '../UI/Toggle.svelte';
+	import Button from '../UI/Button.svelte';
+	import { RefreshCW } from 'svelte-pangolicons';
 
 	import { theme, toggleColourTheme } from '../../stores/theme.store';
 </script>
@@ -13,14 +16,16 @@
 			title="Dark Theme"
 			id="theme"
 			checked={$theme === 'dark'}
-			on:change={toggleColourTheme}
 			description="Toggle between dark & light theme"
+			on:change={toggleColourTheme}
 		/>
-		<Toggle
-			title="Hard Mode"
-			id="difficulty"
-			checked={false}
-			description="Any revealed hints must be used in subsequent guesses"
-		/>
+		<Button
+			title="Get a new Wordle"
+			id="refresh"
+			description="Waiting 24 hours is boring"
+			on:click={resetGameState}
+		>
+			<RefreshCW />
+		</Button>
 	</Modal>
 {/if}
