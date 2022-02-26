3f6fa585e17788f3bf0ba02e40daad8a89f56a15
Sebastian <sebastian@iamsebastian.dev>
2022-02-27 00:09:57 +0100

    FEAT: Added a hardmode, refractored settings

--- src/lib/components/Modals/Settings.svelte
+++ src/lib/components/Modals/Settings.svelte
@@ -1,13 +1,20 @@
 <script>
 	import Modal from '../UI/Modal.svelte';
 	import { activeModal, closeModal } from '../../stores/modal.store.js';
-	import { resetGameState } from '../../stores/gamestate.store.js';
+	import { gameState } from '../../stores/gamestate.store.js';
+	import { theme } from '../../stores/theme.store';
 
 	import Toggle from '../UI/Toggle.svelte';
 	import Button from '../UI/Button.svelte';
 	import { RefreshCW } from 'svelte-pangolicons';
 
-	import { theme, toggleColourTheme } from '../../stores/theme.store';
+	const toggleHardMode = (ev) => {
+		gameState.dispatch({ type: 'mode', payload: ev.target.checked });
+	};
+
+	const toggleTheme = (ev) => {
+		theme.update((state) => (state === 'dark' ? 'light' : 'dark'));
+	};
 </script>
 
 {#if $activeModal === 0}
@@ -17,15 +24,27 @@
 			id="theme"
 			checked={$theme === 'dark'}
 			description="Toggle between dark & light theme"
-			on:change={toggleColourTheme}
+			on:change={toggleTheme}
+		/>
+		<Toggle
+			title="Hard Mode"
+			id="theme"
+			checked={$gameState.hardmode}
+			description="Any revealed hints must be used in subsequent guesses"
+			on:change={toggleHardMode}
 		/>
 		<Button
 			title="Get a new Wordle"
 			id="refresh"
 			description="Waiting 24 hours is boring"
-			on:click={resetGameState}
+			on:click={() => gameState.dispatch({ type: 'reset' })}
 		>
-			<RefreshCW />
+			<RefreshCW class="motion-safe:animate-spin" />
 		</Button>
+		<a
+			class="w-full p-4 text-center underline"
+			href="https://github.com/IamSebastianDev/woerdle"
+			rel="noreferrer noopener">Find this Project on Github</a
+		>
 	</Modal>
 {/if}
