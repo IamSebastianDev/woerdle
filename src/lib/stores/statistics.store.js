8e0430edbfbea16540a5ec40299b2d114094cd0e
Sebastian <sebastian@iamsebastian.dev>
2022-02-27 00:11:46 +0100

    PROG: Refractored and cleaned up the game's statistic store

--- src/lib/stores/statistics.store.js
+++ src/lib/stores/statistics.store.js
@@ -1,56 +1,52 @@
 /** @format */
 
-import { writable } from 'svelte/store';
-import { getDefaultStats } from '../data/defaults.js';
+import { persistable } from '../scripts/persistable.util.js';
+import { reduceable } from '../scripts/reducable.util.js';
 
-const savedStats = JSON.parse(window.localStorage.getItem('wordle-statistics'));
-const stats = writable(savedStats || getDefaultStats());
-
-stats.subscribe((state) =>
-	localStorage.setItem('wordle-statistics', JSON.stringify(state))
-);
-
-const calcAverage = (guesses, gamesPlayed) => {
-	const numberOfGuesses = Object.values(guesses).slice(0, 6);
-	return numberOfGuesses.reduce((a, b, i) => a + b * i + 1, 0) / gamesPlayed;
+const initializeStats = () => {
+	return {
+		currentStreak: 0,
+		gamesPlayed: 0,
+		gamesWon: 0,
+		guesses: {
+			1: 0,
+			2: 0,
+			3: 0,
+			4: 0,
+			5: 0,
+			6: 0,
+			failed: 0,
+		},
+		maxStreak: 0,
+	};
 };
 
-const updateStats = ({ type, payload = '' }) => {
+const statisticsReducer = (state, { type, payload = '' }) => {
+	const newState = { ...state };
 	switch (type) {
 		case 'win':
-			stats.update((state) => {
-				const newState = { ...state };
+			newState.guesses[payload] = state.guesses[payload] + 1;
+			newState.gamesWon = state.gamesWon + 1;
+			newState.gamesPlayed = state.gamesPlayed + 1;
+			newState.currentStreak = state.currentStreak + 1;
+			newState.maxStreak =
+				newState.currentStreak > state.maxStreak
+					? newState.currentStreak
+					: newState.maxStreak;
 
-				newState.guesses[payload] = state.guesses[payload] + 1;
-				newState.gamesWon = state.gamesWon + 1;
-				newState.gamesPlayed = state.gamesPlayed + 1;
-				newState.currentStreak = state.currentStreak + 1;
-				newState.averageGuesses = calcAverage(
-					newState.guesses,
-					newState.gamesPlayed
-				);
-				newState.maxStreak =
-					newState.currentStreak > state.maxStreak
-						? newState.currentStreak
-						: newState.maxStreak;
-
-				return newState;
-			});
-			break;
+			return newState;
 		case 'loss':
-			stats.update((state) => {
-				const newState = { ...state };
+			newState.guesses.failed = state.guesses.failed + 1;
+			newState.gamesPlayed = state.gamesPlayed + 1;
+			newState.currentStreak = 0;
 
-				newState.guesses.failed = state.guesses.failed + 1;
-				newState.gamesPlayed = state.gamesPlayed + 1;
-				newState.currentStreak = 0;
-
-				return newState;
-			});
-			break;
+			return newState;
 		default:
 			break;
 	}
+
+	return newState;
 };
 
-export { stats, updateStats };
+const persistantStats = persistable(initializeStats(), 'wordle-stats');
+export const stats = reduceable(statisticsReducer, persistantStats);
