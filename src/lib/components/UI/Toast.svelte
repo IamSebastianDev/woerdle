2b5d949f2e098ea78569165100a1ba2a81133d25
Sebastian <sebastian@iamsebastian.dev>
2022-02-27 01:01:50 +0100

    FIX: Fixed a issue where the toast message would be displayed at the wrong position

--- src/lib/components/UI/Toast.svelte
+++ src/lib/components/UI/Toast.svelte
@@ -8,7 +8,7 @@
 	<div
 		in:fade={{ easing: quintInOut, duration: 150 }}
 		out:fade={{ easing: quintInOut, duration: 350 }}
-		class="absolute top-24 z-50 rounded-md border border-zinc-700 bg-zinc-700 py-3 px-4 font-bold text-zinc-200 shadow-md dark:bg-zinc-200 dark:text-zinc-700"
+		class="absolute -top-16 z-50 rounded-md border border-zinc-700 bg-zinc-700 py-3 px-4 font-bold text-zinc-200 shadow-md dark:bg-zinc-200 dark:text-zinc-700 sm:top-10"
 	>
 		{$toast.message}
 	</div>
