a736f99082c56dbe6e0ac228e312c9fa14efbdb4
Sebastian <sebastian@iamsebastian.dev>
2022-03-01 17:54:12 +0100

    PROG: Refractored modalstore

--- src/lib/stores/modal.store.js
+++ src/lib/stores/modal.store.js
@@ -1,21 +1,25 @@
 /** @format */
 
 import { writable } from 'svelte/store';
+import { persistable } from '../scripts/persistable.util';
+import { reduceable } from '../scripts/reducable.util';
 
-// modal index:
-//      settings: 		0,
-//      instructions: 	1,
-//      stats: 			2
-
-export const activeModal = writable(null);
-export const overlay = writable(false);
-
-export const openModal = (index, useOverlay = false) => {
-	activeModal.set(index);
-	overlay.set(useOverlay);
+const modalStore = writable({ active: null, overlay: false });
+const modalReducer = (state, { type, payload }) => {
+	const { overlay } = payload || false;
+	switch (type) {
+		case 'close':
+			return { active: null, overlay };
+		case 'stats':
+			return { active: 'Stats', overlay };
+		case 'instructions':
+			return { active: 'Instructions', overlay };
+		case 'settings':
+			return { active: 'Settings', overlay };
+		default:
+			return state;
+	}
 };
 
-export const closeModal = () => {
-	activeModal.set(null);
-	overlay.set(false);
-};
+export const modal = reduceable(modalReducer, modalStore);
+export const initalModal = persistable({ show: true }, 'wordle-initalize');
