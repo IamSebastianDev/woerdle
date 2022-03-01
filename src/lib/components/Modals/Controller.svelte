7b88082174e4a57a7ba26cc92750b9d46ac6bd18
Sebastian <sebastian@iamsebastian.dev>
2022-03-01 17:53:46 +0100

    PROG: Created a Modalcontroller component

---
+++ src/lib/components/Modals/Controller.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Settings from './Settings.svelte';
+	import Stats from './Stats.svelte';
+	import Instructions from './Instructions.svelte';
+	import { modal, initalModal } from '../../stores/modal.store';
+	import { onMount } from 'svelte';
+
+	let modals = { Settings, Stats, Instructions };
+
+	/**
+	 * On pageload, check if the instrucionmodal should be shown and dispatch a
+	 * opening request if ncessary.
+	 */
+
+	onMount(() => {
+		$initalModal.show &&
+			modal.dispatch({
+				type: 'instructions',
+				payload: { overlay: true },
+			});
+	});
+</script>
+
+<section id="modals">
+	<svelte:component this={modals[$modal.active]} />
+</section>
