64e20f8c44ba92452957663c2ab3af8e60b2a807
Sebastian <sebastian@iamsebastian.dev>
2022-03-06 18:19:42 +0100

    FEAT: Added a de & en dictionary

---
+++ src/lib/i18n/dicts/de.dict.js
@@ -0,0 +1,28 @@
+/** @format */
+
+export default {
+	locale: 'German',
+	code: 'de',
+	phrases: {
+		settings: {
+			heading: 'Einstellungen',
+			localeSelect: {
+				heading: 'Sprache',
+				description: 'Wähle zwischen deutscher und englischer sprache',
+			},
+			hardMode: {
+				heading: 'Schwerer Modus',
+				description:
+					'Alle geratenen Buchstaben müssen beim nächsten Raten benutzt werden',
+			},
+			theme: {
+				heading: 'Theme',
+				description: 'Wähle zwischen einem dunklen und hellen Theme',
+			},
+			links: {
+				github: 'Find this Project on Github',
+				twitter: 'Find me on Twitter',
+			},
+		},
+	},
+};
---
+++ src/lib/i18n/dicts/en.dict.js
@@ -0,0 +1,41 @@
+/** @format */
+
+export default {
+	locale: 'English',
+	code: 'en',
+	phrases: {
+		settings: {
+			heading: 'Settings',
+			localeSelect: {
+				heading: 'Language',
+				description:
+					'You can change the language between English and German.',
+			},
+			hardMode: {
+				heading: 'Hard Mode',
+				description:
+					'Any revealed hints must be used in subsequent guesses',
+			},
+			theme: {
+				heading: 'Dark Theme',
+				description: 'Toggle between dark & light theme',
+			},
+			links: {
+				github: 'Find this Project on Github',
+				twitter: 'Find me on Twitter',
+			},
+		},
+		instructions: {
+			heading: 'How to play',
+			line1: 'Guess the <strong>WORDLE</strong> in six tries.',
+			line2: 'Each guess must be a valid five-letter word. Hit the enter button to submit.',
+			line3: 'After each guess, the color of the tiles will change to show how close your guess was to the word.',
+			examples: {
+				heading: 'Examples',
+				line1: 'The letter S is in the word and in the correct spot.',
+				line2: 'The letter H is in the word but in the wrong spot.',
+				line3: 'The letter R is not in the word in any spot.',
+			},
+		},
+	},
+};
