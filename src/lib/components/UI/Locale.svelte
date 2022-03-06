1e085ec5a9711d427b32b4b2d58f31bd45523e43
Sebastian <sebastian@iamsebastian.dev>
2022-03-06 21:28:43 +0100

    FEAT: Added a locale switch component

---
+++ src/lib/components/UI/Locale.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { i18n } from '../../i18n/i18n.store';
+	$: ({ currentLocale } = i18n);
+</script>
+
+<div
+	class="group flex w-full flex-row items-center justify-between border-b border-b-zinc-700 py-5 text-lg font-bold"
+>
+	<slot />
+	<div class="flex flex-row divide-x text-base">
+		<button
+			class="px-2 {$currentLocale === 'en'
+				? 'text-amber-500'
+				: 'text-inherit'}"
+			on:click={currentLocale.set('en')}>EN</button
+		>
+		<button
+			class="px-2 {$currentLocale === 'de'
+				? 'text-amber-500'
+				: 'text-inherit'}"
+			on:click={currentLocale.set('de')}>DE</button
+		>
+	</div>
+</div>
