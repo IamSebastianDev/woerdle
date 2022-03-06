fc91aff37a02b299205accfe4276e99f901a0006
Sebastian <sebastian@iamsebastian.dev>
2022-03-06 21:29:27 +0100

    FIX: Fixed processPhrase method to correctly replace escaped letters

--- src/lib/i18n/utils/processPhrase.util.ts
+++ src/lib/i18n/utils/processPhrase.util.ts
@@ -3,8 +3,11 @@
 import type { Entry, Processor } from '../types';
 
 export const processPhrase: Processor = (phrase, data) => {
-	return phrase.replaceAll(/{{(.*?)}}/gim, (r) =>
-		/// @ts-ignore
-		r.split('.').reduce((key, value) => key[value], data)
+	return phrase.replaceAll(/{{.*?}}/gim, (r) =>
+		r
+			.substring(2, r.length - 2)
+			.split('.')
+			/// @ts-ignore
+			.reduce((key, value) => key[value], data)
 	);
 };
